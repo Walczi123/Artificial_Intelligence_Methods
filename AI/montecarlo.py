a5b01e0a8a5090fe6292084a97d27dc222da4830
Patryk Walczak <patryk.wal@onet.pl>
2021-05-10 13:12:31 +0200

    mcts update

--- AI/montecarlo.py
+++ AI/montecarlo.py
@@ -1,58 +1,32 @@
 import random
 from AI.node import Node
 
+def MCTS( initialState, numberOfIteration):
+    rootnode = Node(initialState)
+    for _ in range(numberOfIteration):
+        node = rootnode
+        iteration_state = node.state
 
-class MonteCarloTreeSearch:
-    def __init__(self):
-        pass
+        #Selection
+        while node.untriedMoves == [] and node.childNodes != []:
+            node = node.SelectUctChild()
 
-    def selection(self):
-        best_children = []
-        best_score = float('-inf')
+        #Expansion         
+        if node.untriedMoves != []:
+            move = random.choice(node.untried_moves)
+            iteration_state.do_move(move)
+            node = node.AddChild(iteration_state, move)
 
-        for child in self.children:
-            score = child.select_UTC_child()
-
-            if score > best_score:
-                best_score = score
-                best_children = [child]
-            elif score == best_score:
-                best_children.append(child)
-
-        return random.choice(best_children)
-
-    def expansion(self, node):
-        if node.untried_moves != []:
-            m = random.choice(node.untried_moves)
-            state.make_move(m)
-            node = node.add_child(m, state)
-
-    def playout(self):
+        #Playout
         while True:
-            all_possible_moves = state.get_all_possible_moves()
-            if all_possible_moves != []:
-                state.make_move(random.choice(all_possible_moves))
-                continue
-            state.chg_color()
-            all_possible_moves = state.get_all_possible_moves()
-            if all_possible_moves != []:
-                for corner in [0, 7, 56, 63]:
-                    if corner in all_possible_moves:
-                        state.make_move(corner)
-                        continue
-                state.make_move(random.choice(all_possible_moves))
-                continue
-            break
-
-    def backpropagation(self, result):
-        self.win_value += result
-        self.visits += 1
-
-        if self.parent:
-            self.parent.backpropagation(result)
-
-    def get_move(self, state):
-        node = self.selection()
-        self.expansion(node)
-        self.playout()
-        self.backpropagation()
+            all_possible_moves = GetAllPosibleMoves(iteration_state)
+            if all_possible_moves == []:
+                break
+            move = random.choice(all_possible_moves)
+            iteration_state = StateAfterMove(iteration_state, move)
+        
+        #Backpropagation
+        result = GetResult(iterationState)
+        node.Backpropagation(result)
+    
+    return sorted(rootnode.child_nodes, key = lambda c: c.visits)[-1].move
