28682cda7fd516beadd8f9b5df81c0ac83e01412
Patryk <patryk.wal@onet.pl>
2021-05-10 16:59:04 +0200

    ai updates

--- AI/MCTS.py
+++ AI/MCTS.py
@@ -1,32 +1,48 @@
 import random
-from AI.node import Node
+from AI.nodes import Node
 
-def MCTS( initialState, numberOfIteration):
+
+def select_uct_child(childNodes):
+    bestChildren = list()
+    bestScore = - float("inf")
+
+    for childNode in childNodes:
+        score = childNode.get_uct_score()
+        if score > bestScore:
+            bestScore = score
+            bestChildren = {childNode}
+        elif score == bestScore:
+            bestChildren.append(childNode)
+
+    return bestChildren[random.Next(bestChildren.Count)]
+
+
+def MCTS(initialState, numberOfIteration):
     rootnode = Node(initialState)
     for _ in range(numberOfIteration):
         node = rootnode
         iteration_state = node.state
 
-        #Selection
+        # Selection
         while node.untriedMoves == [] and node.childNodes != []:
-            node = node.SelectUctChild()
+            node = select_uct_child(node.child_nodes)
 
-        #Expansion         
+        # Expansion
         if node.untriedMoves != []:
             move = random.choice(node.untried_moves)
             iteration_state.do_move(move)
             node = node.AddChild(iteration_state, move)
 
-        #Playout
+        # Playout
         while True:
             all_possible_moves = GetAllPosibleMoves(iteration_state)
             if all_possible_moves == []:
                 break
             move = random.choice(all_possible_moves)
             iteration_state = StateAfterMove(iteration_state, move)
-        
-        #Backpropagation
+
+        # Backpropagation
         result = GetResult(iterationState)
         node.Backpropagation(result)
-    
-    return sorted(rootnode.child_nodes, key = lambda c: c.visits)[-1].move
+
+    return sorted(rootnode.child_nodes, key=lambda c: c.visits)[-1].move
