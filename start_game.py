cd6f5c9ed3a088633838253ca8c3e5482ccc5eb1
Alicja <alussiiaa@gmail.com>
2021-06-11 18:40:13 +0200

    Ready to test really

--- start_game.py
+++ start_game.py
@@ -6,7 +6,7 @@ from AI.MCTS_RAVE import MCTS_RAVE
 from AI.MCTS_MAST import MCTS_MAST
 
 if __name__ == "__main__":
-    result = run_game(MCTS_RAVE, MCTS_MAST, True, True, n_iterations=30)
+    result = run_game(MCTS_RAVE, MCTS_MAST,  n_iterations=800, printfinalResult=True, printSteps=True)
     print(result)
     # please, work
     # result = run_game(heu, MCTS)
