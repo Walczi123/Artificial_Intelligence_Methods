9d8e954011c0c06b3e2eba0e4ecc6e3090fe1210
Patryk <patryk.wal@onet.pl>
2021-06-11 16:51:26 +0200

    please work

--- start_game.py
+++ start_game.py
@@ -8,4 +8,5 @@ from AI.MCTS_MAST import MCTS_MAST
 if __name__ == "__main__":
     result = run_game(MCTS_RAVE, MCTS_MAST, True, True, n_iterations=30)
     print(result)
+    # please, work
     # result = run_game(heu, MCTS)
